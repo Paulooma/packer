eb753b6bc4f74365183855ef0c7c30fb9ae9dbb8
sylviamoss <moss@hashicorp.com>
2020-09-10 10:48:34 +0200

    wip - add vnc over websocket

--- builder/vmware/common/step_configure_vnc.go
+++ builder/vmware/common/step_configure_vnc.go
@@ -1,14 +1,19 @@
 package common
 
 import (
+	"bytes"
 	"context"
+	"crypto/tls"
+	"encoding/binary"
 	"fmt"
-	"log"
-	"math/rand"
-
-	"github.com/hashicorp/packer/common/net"
+	"github.com/gorilla/websocket"
+	netCommon "github.com/hashicorp/packer/common/net"
 	"github.com/hashicorp/packer/helper/multistep"
 	"github.com/hashicorp/packer/packer"
+	"github.com/vmware/govmomi/vim25/types"
+	"log"
+	"math/rand"
+	"net/http"
 )
 
 // This step configures the VM to enable the VNC server.
@@ -25,8 +30,10 @@ type StepConfigureVNC struct {
 	VNCPortMin         int
 	VNCPortMax         int
 	VNCDisablePassword bool
+	DriverConfig       *DriverConfig
+	VNCOverWebsocket   bool
 
-	l *net.Listener
+	l *netCommon.Listener
 }
 
 type VNCAddressFinder interface {
@@ -38,7 +45,7 @@ type VNCAddressFinder interface {
 
 func (s *StepConfigureVNC) VNCAddress(ctx context.Context, vncBindAddress string, portMin, portMax int) (string, int, error) {
 	var err error
-	s.l, err = net.ListenRangeConfig{
+	s.l, err = netCommon.ListenRangeConfig{
 		Addr:    s.VNCBindAddress,
 		Min:     s.VNCPortMin,
 		Max:     s.VNCPortMax,
@@ -75,11 +82,55 @@ func (s *StepConfigureVNC) Run(ctx context.Context, state multistep.StateBag) mu
 		log.Println("Skipping VNC configuration step...")
 		return multistep.ActionContinue
 	}
+	ui := state.Get("ui").(packer.Ui)
+
+	if s.VNCOverWebsocket {
+		driver := state.Get("driver").(*ESX5Driver)
+		// Aquire websocket ticket
+		ticket, err := driver.AcquireTicket()
+		if err != nil {
+			err := fmt.Errorf("Error reading VMX file: %s", err)
+			state.Put("error", err)
+			ui.Error(err.Error())
+			return multistep.ActionHalt
+		}
+		log.Printf("MOSS mks ticket %#v", ticket)
+		host := ticket.Host
+		if len(host) == 0 {
+			host = s.DriverConfig.RemoteHost
+		}
+		port := ticket.Port
+		if port == 0 {
+			port = 443
+		}
+
+		//Dial websocket
+		dialer := &websocket.Dialer{
+			Proxy:           http.ProxyFromEnvironment,
+			Subprotocols:    []string{"uint8utf8", "binary", "vmware-vvc", "wmks"},
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		}
+		url := fmt.Sprintf("wss://%s:%d/ticket/%s", host, port, ticket.Ticket)
+		dial, _, err := dialer.Dial(url, nil)
+		if err != nil {
+			state.Put("error", err)
+			ui.Error(err.Error())
+			return multistep.ActionHalt
+		}
+		conn := &ConnWrapper{dial, ticket}
+		err = conn.handshake()
+		if err != nil {
+			state.Put("error", err)
+			ui.Error(err.Error())
+			return multistep.ActionHalt
+		}
+
+		state.Put("vnc_conn", conn)
+		return multistep.ActionContinue
+	}
 
 	driver := state.Get("driver").(Driver)
-	ui := state.Get("ui").(packer.Ui)
 	vmxPath := state.Get("vmx_path").(string)
-
 	vmxData, err := ReadVMX(vmxPath)
 	if err != nil {
 		err := fmt.Errorf("Error reading VMX file: %s", err)
@@ -139,4 +190,94 @@ func (s *StepConfigureVNC) Cleanup(multistep.StateBag) {
 			log.Printf("failed to unlock port lockfile: %v", err)
 		}
 	}
+}
+
+type ConnWrapper struct {
+	c      *websocket.Conn
+	ticket *types.VirtualMachineTicket
+}
+
+// KeyEvent indiciates a key press or release and sends it to the server.
+// The key is indicated using the X Window System "keysym" value. Use
+// Google to find a reference of these values. To simulate a key press,
+// you must send a key with both a down event, and a non-down event.
+//
+// See 7.5.4.
+func (c *ConnWrapper) KeyEvent(keysym uint32, down bool) error {
+	var downFlag uint8 = 0
+	if down {
+		downFlag = 1
+	}
+
+	data := []interface{}{
+		uint8(4),
+		downFlag,
+		uint8(0),
+		uint8(0),
+		keysym,
+	}
+
+	for _, val := range data {
+		buf := new(bytes.Buffer)
+		err := binary.Write(buf, binary.LittleEndian, val)
+		if err != nil {
+			fmt.Println("binary.Write failed:", err)
+		}
+		if err := c.c.WriteMessage(websocket.BinaryMessage, buf.Bytes()); err != nil {
+			log.Printf("MOSS failed to send %#v", val)
+			return err
+		}
+	}
+
+	log.Printf("MOSS reading back %#v", keysym)
+	_, _, err := c.c.ReadMessage()
+	if err != nil {
+		return fmt.Errorf("Error reading back %#v: %s", keysym, err)
+	}
+
+	return nil
+}
+
+func (c *ConnWrapper) Close() error {
+	return c.c.Close()
+}
+
+func (c *ConnWrapper) handshake() error {
+	mt, protocolVersion, err := c.c.ReadMessage()
+	if err != nil {
+		return fmt.Errorf("Error reading message protocol version: %s", err)
+	}
+	log.Printf("MOSS protocolVersion %s", string(protocolVersion))
+
+	log.Printf("MOSS sending back protocol version")
+	err = c.c.WriteMessage(mt, protocolVersion)
+	if err != nil {
+		return fmt.Errorf("Error reading message protocol version: %s", err)
+	}
+
+	log.Printf("MOSS reading security quantity")
+	_, secQty, err := c.c.ReadMessage()
+	if err != nil {
+		return fmt.Errorf("Error reading security quantity: %s", err)
+	}
+	log.Printf("MOSS security quantity %s", string(secQty))
+
+	log.Printf("MOSS sending security type")
+	buf := new(bytes.Buffer)
+	err = binary.Write(buf, binary.LittleEndian, uint8(1))
+	if err != nil {
+		fmt.Println("binary.Write failed:", err)
+	}
+	if err := c.c.WriteMessage(websocket.BinaryMessage, buf.Bytes()); err != nil {
+		return fmt.Errorf("Error sending security type: %s", err)
+	}
+
+	log.Printf("MOSS reading security type")
+	_, serverInit, err := c.c.ReadMessage()
+	if err != nil {
+		return fmt.Errorf("Error reading security type: %s", err)
+	}
+	log.Printf("MOSS server init %s", string(serverInit))
+
+	return nil
 }
